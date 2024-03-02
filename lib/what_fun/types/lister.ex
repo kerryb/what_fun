aee66270426fdfb67592635c4bc685933bc8b297
Kerry Buckley <kerryjbuckley@gmail.com>
2024-03-02 17:25:00 +0000

    Build type list at compile time

---
+++ lib/what_fun/types/lister.ex
@@ -0,0 +1,23 @@
+defmodule WhatFun.Types.Lister do
+  @moduledoc false
+  def all do
+    for module <-
+          Enum.flat_map(:code.all_loaded(), fn {mod, _} -> if to_string(mod) =~ ~r/^[A-Z]/, do: [mod], else: [] end),
+        type <- types_for(module) do
+      %{module: module, type: type}
+    end
+  end
+
+  defp types_for(module) do
+    case Code.Typespec.fetch_types(module) do
+      {:ok, types} ->
+        Enum.flat_map(types, fn
+          {:type, {type, _, _}} -> [type]
+          _ -> []
+        end)
+
+      _ ->
+        []
+    end
+  end
+end
--- lib/what_fun/types/types.ex
+++ lib/what_fun/types/types.ex
@@ -4,24 +4,8 @@ defmodule WhatFun.Types do
   application itself).
   """
 
-  def all do
-    for module <-
-          Enum.flat_map(:code.all_loaded(), fn {mod, _} -> if to_string(mod) =~ ~r/^[A-Z]/, do: [mod], else: [] end),
-        type <- types_for(module) do
-      %{module: module, type: type}
-    end
-  end
+  alias WhatFun.Types.Lister
 
-  defp types_for(module) do
-    case Code.Typespec.fetch_types(module) do
-      {:ok, types} ->
-        Enum.flat_map(types, fn
-          {:type, {type, _, _}} -> [type]
-          _ -> []
-        end)
-
-      _ ->
-        []
-    end
-  end
+  @all Lister.all()
+  def all, do: @all
 end
